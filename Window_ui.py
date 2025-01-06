88ebaee972a5daa344cf946f695df2e52de9cfea
Nicolas <nicolas80d21@gmail.com>
2025-01-06 14:11:05 +0100

    Update Window_ui.py

--- Window_ui.py
+++ Window_ui.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'Window.ui'
+## Form generated from reading UI file 'SearchWindow.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.8.1
 ##
@@ -15,73 +15,80 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QHBoxLayout, QLineEdit, QMainWindow,
-    QPushButton, QScrollArea, QSizePolicy, QVBoxLayout,
-    QWidget)
+from PySide6.QtWidgets import (QApplication, QDialog, QGridLayout, QLabel,
+    QLineEdit, QPushButton, QSizePolicy, QWidget)
 
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        if not MainWindow.objectName():
-            MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(800, 600)
-        MainWindow.setMinimumSize(QSize(400, 4))
-        self.centralwidget = QWidget(MainWindow)
-        self.centralwidget.setObjectName(u"centralwidget")
-        self.horizontalLayout_3 = QHBoxLayout(self.centralwidget)
-        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
-        self.verticalLayout = QVBoxLayout()
-        self.verticalLayout.setObjectName(u"verticalLayout")
-        self.horizontalLayout_2 = QHBoxLayout()
-        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.lineEdit = QLineEdit(self.centralwidget)
-        self.lineEdit.setObjectName(u"lineEdit")
-        self.lineEdit.setLocale(QLocale(QLocale.French, QLocale.France))
+class Ui_SearchEngine(object):
+    def setupUi(self, SearchEngine):
+        if not SearchEngine.objectName():
+            SearchEngine.setObjectName(u"SearchEngine")
+        SearchEngine.resize(632, 377)
+        self.gridLayout = QGridLayout(SearchEngine)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.SearchEdit = QLineEdit(SearchEngine)
+        self.SearchEdit.setObjectName(u"SearchEdit")
 
-        self.horizontalLayout_2.addWidget(self.lineEdit)
+        self.gridLayout.addWidget(self.SearchEdit, 0, 0, 1, 2)
 
-        self.searchButton = QPushButton(self.centralwidget)
-        self.searchButton.setObjectName(u"searchButton")
-        self.searchButton.setLocale(QLocale(QLocale.French, QLocale.France))
+        self.SearchButton = QPushButton(SearchEngine)
+        self.SearchButton.setObjectName(u"SearchButton")
 
-        self.horizontalLayout_2.addWidget(self.searchButton)
+        self.gridLayout.addWidget(self.SearchButton, 0, 2, 1, 1)
+
+        self.Information = QLineEdit(SearchEngine)
+        self.Information.setObjectName(u"Information")
+        self.Information.setMinimumSize(QSize(150, 0))
+
+        self.gridLayout.addWidget(self.Information, 2, 0, 1, 1)
+
+        self.ZoomIn = QPushButton(SearchEngine)
+        self.ZoomIn.setObjectName(u"ZoomIn")
+        self.ZoomIn.setMinimumSize(QSize(75, 0))
+        icon = QIcon()
+        icon.addFile(u"zoom+.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.ZoomIn.setIcon(icon)
+
+        self.gridLayout.addWidget(self.ZoomIn, 2, 1, 1, 1)
+
+        self.ZoomOut = QPushButton(SearchEngine)
+        self.ZoomOut.setObjectName(u"ZoomOut")
+        self.ZoomOut.setEnabled(True)
+        icon1 = QIcon()
+        icon1.addFile(u"zoom-.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.ZoomOut.setIcon(icon1)
+
+        self.gridLayout.addWidget(self.ZoomOut, 2, 2, 1, 1)
+
+        self.Image = QLabel(SearchEngine)
+        self.Image.setObjectName(u"Image")
+        self.Image.setMinimumSize(QSize(200, 0))
+        self.Image.setStyleSheet(u"border: 2px solid black;  /* Cr\u00e9e une bordure noire de 2px autour de l'image */\n"
+"border-radius: 10px;      /* Arrondir les coins du cadre */")
+
+        self.gridLayout.addWidget(self.Image, 1, 0, 1, 1)
+
+        self.label = QLabel(SearchEngine)
+        self.label.setObjectName(u"label")
+        self.label.setMinimumSize(QSize(250, 300))
+        self.label.setStyleSheet(u"border: 2px solid black;  /* Cr\u00e9e une bordure noire de 2px autour de l'image */\n"
+"border-radius: 10px;      /* Arrondir les coins du cadre */")
+
+        self.gridLayout.addWidget(self.label, 1, 1, 1, 2)
 
 
-        self.verticalLayout.addLayout(self.horizontalLayout_2)
+        self.retranslateUi(SearchEngine)
 
-        self.scrollArea = QScrollArea(self.centralwidget)
-        self.scrollArea.setObjectName(u"scrollArea")
-        self.scrollArea.setLocale(QLocale(QLocale.French, QLocale.France))
-        self.scrollArea.setWidgetResizable(True)
-        self.scrollAreaWidgetContents = QWidget()
-        self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
-        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 778, 546))
-        self.horizontalLayout = QHBoxLayout(self.scrollAreaWidgetContents)
-        self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.widget = QWidget(self.scrollAreaWidgetContents)
-        self.widget.setObjectName(u"widget")
-        self.widget.setLocale(QLocale(QLocale.French, QLocale.France))
-
-        self.horizontalLayout.addWidget(self.widget)
-
-        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
-
-        self.verticalLayout.addWidget(self.scrollArea)
-
-
-        self.horizontalLayout_3.addLayout(self.verticalLayout)
-
-        MainWindow.setCentralWidget(self.centralwidget)
-
-        self.retranslateUi(MainWindow)
-
-        QMetaObject.connectSlotsByName(MainWindow)
+        QMetaObject.connectSlotsByName(SearchEngine)
     # setupUi
 
-    def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.lineEdit.setInputMask("")
-        self.lineEdit.setText("")
-        self.lineEdit.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Ins\u00e9rez quelques mots pour rechercher des images\u2026", None))
-        self.searchButton.setText(QCoreApplication.translate("MainWindow", u"Rechercher", None))
+    def retranslateUi(self, SearchEngine):
+        SearchEngine.setWindowTitle(QCoreApplication.translate("SearchEngine", u"Dialog", None))
+        self.SearchEdit.setText(QCoreApplication.translate("SearchEngine", u"Enter your query here", None))
+        self.SearchButton.setText(QCoreApplication.translate("SearchEngine", u"Search", None))
+        self.Information.setText(QCoreApplication.translate("SearchEngine", u"Search information", None))
+        self.ZoomIn.setText("")
+        self.ZoomOut.setText("")
+        self.Image.setText("")
+        self.label.setText("")
     # retranslateUi
 
